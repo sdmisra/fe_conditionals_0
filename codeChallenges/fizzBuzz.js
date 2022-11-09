896fdb52ba5ce82f13e8ceec1e4e6dc737d17da4
Shane Misra <shane.misra@gmail.com>
2022-11-09 12:49:09 -0700

    Finish selfEval.md

--- codeChallenges/fizzBuzz.js
+++ codeChallenges/fizzBuzz.js
@@ -2,7 +2,7 @@
 
 var number = 15
 
-if ((number % 3) === 0 && (number % 5) === 0 ){
+if (((number % 3) === 0) && ((number % 5) === 0 )){
     console.log('FizzBuzz')
 }
 else if ((number % 3)=== 0){
